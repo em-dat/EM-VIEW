36da6726db7c16078a003d2f08c992e123a93f5e
Damien Delforge <damien.delforge@uclouvain.be>
2024-03-27 08:43:43 +0100

    Initial commit

--- EMVIEW.py
+++ EMVIEW.py
@@ -1,16 +1,11 @@
 import io
 from datetime import date
-
+from utils.layout import init_layout
 import pandas as pd
 import streamlit as st
 
 from utils.sidebar import init_filters, filter_data
 
-st.set_page_config(page_title="EM-VIEW", page_icon="🌍", layout='wide',
-                   menu_items={
-                       'Get Help': 'https://www.extremelycoolapp.com/help',
-                       'Report a bug': "https://www.extremelycoolapp.com/bug",
-                       'About': "# This is a header. This is an *extremely* cool app!"})
 
 @st.cache_data
 def load_data(file):
@@ -23,23 +18,20 @@ def load_data(file):
 # App
 # ----
 
+init_layout()
+
 if "data" not in st.session_state:
     st.session_state["data"] = None
-    st.session_state["region_data"] = None
-    st.session_state["columns"] = None
-    st.session_state["start"] = 2000
-    st.session_state["end"] = date.today().year
-    st.session_state["clkey"] = ""
-    st.session_state["region"] = None
-    st.session_state["subregion"] = None
-    st.session_state["country"] = None
+    st.session_state['is_filter'] = False
 
 st.write("# EM-VIEW Disaster Dashboard")
 
 st.write("""
 **EM-VIEW** provides summarization and vizualisation features for your 
-[EM-DAT](https://www.emdat.be) dataset. The **EM-DAT International Disaster 
-Database** contains data about **:earth_africa: global :collision: disaster 
+[**EM-DAT**](https://www.emdat.be) **International Disaster 
+Database** dataset. 
+
+EM-DAT contains data about **:earth_africa: global :collision: disaster 
 occurrence and impact**. 
 1. EM-DAT data is freely accessible for non-commercial use after registration at 
 [public.emdat.be](https://public.emdat.be). 
@@ -55,25 +47,33 @@ uploaded_file = st.file_uploader("Upload your EM-DAT xlsx file...")
 if uploaded_file is not None:
     data, metadata = load_data(uploaded_file)
     st.session_state['data'] = data
-    st.session_state['start'] = int(data['Start Year'].min())
-    st.session_state['end'] = int(data['Start Year'].max())
-    st.session_state['region_data'] = data[
-        ['Region', 'Subregion', 'Country']].drop_duplicates()
     st.session_state['metadata'] = metadata
     st.session_state['filename'] = uploaded_file.name
-    filter_data()
 
-st.sidebar.subheader('Filters')
+# st.sidebar.subheader('Filters')
 
 if st.session_state['data'] is not None:
-    data = st.session_state['data']
     st.success("File upload successful")
+
+    if not st.session_state['is_filter']:
+        st.session_state['filter.start'] = data['Start Year'].min()
+        st.session_state['filter.end'] = data['Start Year'].max()
+        st.session_state["filter.clkey"] = ''
+        st.session_state["filter.region"] = None
+        st.session_state["filter.subregion"] = None
+        st.session_state["filter.country"] = None
+        st.session_state['region_data'] = data[
+            ['Region', 'Subregion', 'Country']].drop_duplicates()
+        st.session_state['is_filter'] = True
+
+    init_filters()
+    filter_data()
+
     exp1 = st.expander('**Medatata**')
     exp1.write(st.session_state['filename'])
     exp1.write(st.session_state['metadata'])
+    data = st.session_state['data']
     buffer = io.StringIO()
     data.info(buf=buffer)
     s = buffer.getvalue()
     exp1.text(s)
-    init_filters()
-    #st.write(st.session_state)
--- utils/sidebar.py
+++ utils/sidebar.py
@@ -4,12 +4,6 @@ import streamlit as st
 
 def init_filters():
     # Get session state variables
-    start_year = int(st.session_state['start'])
-    end_year = int(st.session_state['end'])
-    clkey = st.session_state['clkey']
-    region = st.session_state['region']
-    subregion = st.session_state['subregion']
-    country = st.session_state['country']
     region_list = [None] + sorted(
         st.session_state["region_data"]['Region'].unique())
     subregion_list = [None] + sorted(
@@ -18,46 +12,57 @@ def init_filters():
 
     # Set up filters
     col1, col2 = st.sidebar.columns(2)
-    col1.number_input(label='**Start year**', min_value=1900,
-                      max_value=date.today().year, value=start_year,
-                      key='start', on_change=filter_data)
-    col2.number_input(label='**End year**', min_value=1900, value=end_year,
-                      max_value=date.today().year, key='end',
+    col1.number_input(label='**Start year**',
+                      min_value=st.session_state['data']['Start Year'].min(),
+                      #value=st.session_state['filter.start'],
+                      max_value=date.today().year,
+                      key='filter.start', on_change=filter_data)
+    col2.number_input(label='**End year**',
+                      min_value=st.session_state['data']['Start Year'].min(),
+                      value=st.session_state['filter.end'],
+                      max_value=date.today().year, key='filter.end',
                       on_change=filter_data)
 
-    st.sidebar.text_input(label="**Classification Key**", value=clkey,
-                          key="clkey",
+    st.sidebar.text_input(label="**Classification Key**",
+                          value=st.session_state['filter.clkey'],
+                          key="filter.clkey",
                           on_change=filter_data,
                           help="Enter the key or its initial part to filter")
     st.sidebar.caption(
         "_Check classification keys [here](https://doc.emdat.be/docs/data-structure-and-content/disaster-classification-system/#main-classification-tree)._")
 
     st.sidebar.selectbox(label="**Region**", options=region_list,
-                         index=region_list.index(region), key="region",
+                         index=region_list.index(st.session_state['filter.region']),
+                         key="filter.region",
                          format_func=lambda x: 'All' if x is None else x,
                          on_change=update_region)
 
     st.sidebar.selectbox(label="**Subregion**", options=subregion_list,
-                         index=subregion_list.index(subregion), key="subregion",
+                         index=subregion_list.index(st.session_state['filter.subregion']),
+                         key="filter.subregion",
                          format_func=lambda x: 'All' if x is None else x,
                          on_change=update_subregion)
 
     st.sidebar.selectbox(label="**Country**", options=country_list,
-                         index=country_list.index(country), key="country",
+                         index=country_list.index(st.session_state['filter.country']),
+                         key="filter.country",
                          format_func=lambda x: 'All' if x is None else x,
                          on_change=update_country)
 
     st.sidebar.button("Reset", type="primary", on_click=reset_filters)
 
+
 def filter_data():
     data_filtered = st.session_state['data'].copy()
-    pstart, pend = st.session_state['start'], st.session_state['end']
-    clkey = st.session_state['clkey']
-    region = st.session_state["region"]
-    subregion = st.session_state["subregion"]
-    country = st.session_state["country"]
+    pstart, pend = st.session_state['filter.start'], st.session_state[
+        'filter.end']
+    clkey = st.session_state['filter.clkey']
+    region = st.session_state["filter.region"]
+    subregion = st.session_state["filter.subregion"]
+    country = st.session_state["filter.country"]
 
-    data_filtered = data_filtered[(data_filtered['Start Year'] >= pstart) & (data_filtered['End Year'] <= pend)]
+    data_filtered = data_filtered[(data_filtered['Start Year'] >= pstart) & (
+            data_filtered['End Year'] <= pend)]
     data_filtered = data_filtered[
         data_filtered['Classification Key'].str.match(clkey.replace('*', '.*'))]
     if region:
@@ -68,78 +73,62 @@ def filter_data():
         data_filtered = data_filtered[data_filtered['Country'] == country]
 
     st.session_state['data_filtered'] = data_filtered
-    st.session_state['start'] = pstart
-    st.session_state['end'] = pend
-    st.session_state['clkey'] = clkey
-    st.session_state['region'] = region
-    st.session_state['subregion'] = subregion
-    st.session_state['country'] = country
+
 
 def update_region():
     region_data = st.session_state['region_data']
-    region = st.session_state['region']
-    subregion = st.session_state['subregion']
-    country = st.session_state['country']
+    region = st.session_state['filter.region']
+    subregion = st.session_state['filter.subregion']
+    country = st.session_state['filter.country']
     if region:
         valid_data = region_data[region_data['Region'] == region]
         if subregion not in valid_data['Subregion']:
-            st.session_state['subregion'] = None
+            st.session_state['filter.subregion'] = None
         if country not in valid_data['Country']:
-            st.session_state['country'] = None
+            st.session_state['filter.country'] = None
     else:
-        st.session_state['subregion'] = None
-        st.session_state['country'] = None
+        st.session_state['filter.subregion'] = None
+        st.session_state['filter.country'] = None
     filter_data()
+
 
 def update_subregion():
     region_data = st.session_state['region_data']
-    region = st.session_state['region']
-    subregion = st.session_state['subregion']
-    country = st.session_state['country']
+    region = st.session_state['filter.region']
+    subregion = st.session_state['filter.subregion']
+    country = st.session_state['filter.country']
     if subregion:
         valid_data = region_data[region_data['Subregion'] == subregion]
         if region not in valid_data['Region']:
-            st.session_state['region'] = valid_data.iloc[0]['Region']
+            st.session_state['filter.region'] = valid_data.iloc[0]['Region']
         if country not in valid_data['Country']:
-            st.session_state['country'] = None
+            st.session_state['filter.country'] = None
     else:
-        st.session_state['country'] = None
+        st.session_state['filter.country'] = None
     filter_data()
 
 
 def update_country():
     region_data = st.session_state['region_data']
-    region = st.session_state['region']
-    subregion = st.session_state['subregion']
-    country = st.session_state['country']
+    region = st.session_state['filter.region']
+    subregion = st.session_state['filter.subregion']
+    country = st.session_state['filter.country']
     if country:
         valid_data = region_data[region_data['Country'] == country]
         if region not in valid_data['Region']:
-            st.session_state['region'] = valid_data.iloc[0]['Region']
+            st.session_state['filter.region'] = valid_data.iloc[0]['Region']
         if subregion not in valid_data['Subregion']:
-            st.session_state['subregion'] = valid_data.iloc[0]['Subregion']
+            st.session_state['filter.subregion'] = valid_data.iloc[0][
+                'Subregion']
     filter_data()
+
 
 def reset_filters():
     data = st.session_state['data']
-    st.session_state['start'] = int(data['Start Year'].min())
-    st.session_state['end'] = int(data['Start Year'].max())
-    st.session_state["clkey"] = ""
-    st.session_state["region"] = None
-    st.session_state["subregion"] = None
-    st.session_state["country"] = None
+    st.session_state['filter.start'] = data['Start Year'].min()
+    st.session_state['filter.end'] = data['Start Year'].max()
+    st.session_state["filter.clkey"] = ''
+    st.session_state["filter.region"] = None
+    st.session_state["filter.subregion"] = None
+    st.session_state["filter.country"] = None
     filter_data()
-
-def add_logo():
-    #     st.markdown("""
-    #         <style>
-    #             [data-testid="stSidebarNav"] {
-    #                 background-image: url(https://www.emdat.be/images/emdat.svg);
-    #                 background-repeat: no-repeat;
-    #                 background-size: 300px;
-    #                 padding-top: 20px;
-    #                 background-position: 20px 20px;
-    #             }
-    #         </style>
-    #         """, unsafe_allow_html=True, )
-    pass
